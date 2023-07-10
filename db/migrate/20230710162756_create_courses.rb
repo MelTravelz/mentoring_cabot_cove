1772faea1ff3994a06911c3d5b4e146977cbb5f1
Melony Franchini <meltravelz@gmail.com>
2023-07-10 10:46:14 -0600

    Add course associations & update schema

--- db/migrate/20230710162756_create_courses.rb
+++ db/migrate/20230710162756_create_courses.rb
@@ -1,7 +1,7 @@
 class CreateCourses < ActiveRecord::Migration[7.0]
   def change
     create_table :courses do |t|
-      t.string :title
+      t.string :name
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_10_162937) do
   enable_extension "plpgsql"
 
   create_table "courses", force: :cascade do |t|
-    t.string "title"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
