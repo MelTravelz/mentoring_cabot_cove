bbf42df22fc335c1a471ad5f9dc32d68d390c6de
Melony Franchini <meltravelz@gmail.com>
2023-07-10 10:44:48 -0600

    Add resident associations & validations

--- spec/models/resident_spec.rb
+++ spec/models/resident_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 RSpec.describe Resident, type: :model do
 
   describe "validations" do
-    it {should validate_presence_of :name}
-    it {should validate_presence_of :age}
-    it {should validate_presence_of :occupation}
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :age }
+    it { should validate_presence_of :occupation }
   end
 
   describe "relationships" do
